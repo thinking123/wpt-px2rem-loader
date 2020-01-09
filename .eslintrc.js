435c5b19add3d32637f0bdb999cf42ea1e449455
liangqing <liangq@weipaitang.com>
2020-01-09 14:26:53 +0800

    esint

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+	'env': {
+		'commonjs': true,
+		'es6': true,
+		'node': true
+	},
+	'extends': 'eslint:recommended',
+	'globals': {
+		'Atomics': 'readonly',
+		'SharedArrayBuffer': 'readonly'
+	},
+	"ignorePatterns": ["test/", "node_modules/"],
+	'parserOptions': {
+		'ecmaVersion': 6
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
+};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-const loaderUtils = require("loader-utils");
-const objectAssign = require("object-assign");
-const px2rem = require("./main");
+const loaderUtils = require('loader-utils');
+const objectAssign = require('object-assign');
+const px2rem = require('./main');
 const defaultConfig = {
-  rem: 10,
-  exclude:[]
+	rem: 10,
+	exclude:[]
 };
 
-module.exports = function(content, map, meta) {
-  const options = loaderUtils.getOptions(this);
-  const config = objectAssign({}, defaultConfig, options);
+module.exports = function(content, map) {
+	const options = loaderUtils.getOptions(this);
+	const config = objectAssign({}, defaultConfig, options);
 
-  return px2rem(content, map, config);
+	return px2rem(content, map, config);
 };
